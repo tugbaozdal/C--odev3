32ce637528bf71ace8e811b498b493c409476d62
Tuğba Sena ÖZDAL <117162651+tugbaozdal@users.noreply.github.com>
2023-11-22 22:47:52 +0300

    labirent üzerinde yol bulma uygulaması
    labirent üzerinde yol bulma uygulaması

---
+++ odev2/odev2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odev2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] dizi = { {0,0,1,1,1,1,1,1},
+                            {1,0,1,1,1,1,1,1},
+                            {1,0,1,1,0,0,0,0},
+                            {1,0,1,1,0,1,1,0},
+                            {1,0,1,1,0,1,1,0},
+                            {1,0,0,0,0,1,1,0},
+                            {1,1,1,1,1,1,1,0},
+                            {1,1,1,1,1,1,1,0},
+            };
+            int yolUzunluk = 0;
+            for (int i =0; i < 8; i++)
+            {
+                for(int j = 0; j < 8; j++)
+                {
+                    if (dizi[i, j] == 0)
+                    {
+                        Console.Write(dizi[i, j]+" ");
+                        yolUzunluk++;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("yol uzunluğu: " + yolUzunluk);
+            
+            Console.ReadLine();
+
+                
+        }
+    }
+}
